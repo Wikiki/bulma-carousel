784dbac75c65d544af7fcfe24288fc7cc8c59d5c
Wikiki <wikiki@protonmail.com>
2018-06-14 12:58:36 +0200

    build(Fix build process and publish):

--- test/index.test.js
+++ test/index.test.js
@@ -4,18 +4,18 @@ const bulmaCarousel = require('../src/js/index').default;
 
 describe('bulmaCarousel', () => {
   test('Should throw exception if instanciate with no/wrong selector', () => {
-    // expect(() => {
-    //   new bulmaCarousel();
-    // }).toThrow('An invalid selector or non-DOM node has been provided.');
+    expect(() => {
+      new bulmaCarousel();
+    }).toThrow('An invalid selector or non-DOM node has been provided.');
   });
 
-  // test('Should return an array', () => {
-  //   var instances = bulmaCarousel.attach('.selector');
-  //   expect(Array.isArray(instances)).toBe(true);
-  // });
+  test('Should return an array', () => {
+    var instances = bulmaCarousel.attach('.selector');
+    expect(Array.isArray(instances)).toBe(true);
+  });
 
-  // test('Should return an array of bulmaCarousel instances', () => {
-  //   var instances = bulmaCarousel.attach();
-  //   instances.every(i => expect(i).toBeInstanceOf(bulmaCarousel));
-  // });
+  test('Should return an array of bulmaCarousel instances', () => {
+    var instances = bulmaCarousel.attach();
+    instances.every(i => expect(i).toBeInstanceOf(bulmaCarousel));
+  });
 });
