85a24c16f67d84c66cdcd7f9aa6096ccf6a4607f
Wikiki <wikiki@protonmail.com>
2018-06-14 15:14:22 +0200

    build:

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,9 @@ const cleancss            = require('gulp-clean-css');
 const colors              = require('ansi-colors');
 const concat              = require('gulp-concat');
 const del                 = require('del');
+const fs				  = require('fs');
 const log                 = require('fancy-log');
+const nop                  = require('gulp-nop');
 const postcss             = require('gulp-postcss');
 const sass                = require('gulp-sass');
 const uglify              = require('gulp-uglify');
@@ -56,26 +58,34 @@ const config = {
 // Uses Sass compiler to process styles, adds vendor prefixes, minifies, then
 // outputs file to the appropriate location.
 gulp.task('build:styles', function() {
-	return gulp
-		.src(config.sass.dependencies.concat([config.sass.source + config.sass.input]))
-		.pipe(concat(config.sass.output.filename + '.sass'))
-		.pipe(sass({
-			style: config.sass.output.format,
-			trace: true,
-			loadPath: [config.sass.source],
-			includePaths: ['node_modules/bulma/sass/utilities/']
-		}))
-		.pipe(concat(config.sass.output.filename + (config.sass.output.format === 'compressed' ? '.min' : '') + '.css'))
-		.pipe(postcss([autoprefixer({browsers: pkg.broswers})]))
-		.pipe(cleancss())
-		.pipe(gulp.dest(config.sass.destination));
+	if (fs.existsSync(config.sass.source + config.sass.input)) {
+		return gulp
+			.src(config.sass.dependencies.concat([config.sass.source + config.sass.input]))
+			.pipe(concat(config.sass.output.filename + '.sass'))
+			.pipe(sass({
+				style: config.sass.output.format,
+				trace: true,
+				loadPath: [config.sass.source],
+				includePaths: ['node_modules/bulma/sass/utilities/']
+			}))
+			.pipe(concat(config.sass.output.filename + (config.sass.output.format === 'compressed' ? '.min' : '') + '.css'))
+			.pipe(postcss([autoprefixer({browsers: pkg.broswers})]))
+			.pipe(cleancss())
+			.pipe(gulp.dest(config.sass.destination));
+	} else {
+		return gulp.src('.').pipe(nop());
+	}
 });
 
 // Copy original sass file to dist
 gulp.task('build:styles:copy', function() {
-	return gulp.src(config.sass.source + config.sass.input)
-		.pipe(concat(config.sass.output.filename + '.sass'))
-		.pipe(gulp.dest(config.sass.destination));
+	if (fs.existsSync(config.sass.source + config.sass.input)) {
+		return gulp.src(config.sass.source + config.sass.input)
+			.pipe(concat(config.sass.output.filename + '.sass'))
+			.pipe(gulp.dest(config.sass.destination));
+	} else {
+		return gulp.src('.').pipe(nop());
+	}
 });
 
 gulp.task('clean:styles', function() {
@@ -94,39 +104,43 @@ gulp.task('clean:styles', function() {
 // Concatenates and uglifies global JS files and outputs result to the
 // appropriate location.
 gulp.task('build:scripts', function() {
-	return gulp
-		.src(config.javascript.source + config.javascript.input)
-		.pipe(webpackStream({
-			output: {
-				filename: config.javascript.output.filename + '.js',
-				library: config.javascript.output.name,
-				libraryTarget: config.javascript.output.format,
-				libraryExport: 'default'
-			},
-			module: {
-				rules: [
-					{
-						test: /\.(js|jsx)$/,
-						exclude: /(node_modules)/,
-						loader: 'babel-loader',
-						options: {
-							babelrc: './babelrc'
-						}
-					},
-				],
-			}
-		}), webpack)
-		.pipe(concat(config.javascript.output.filename + '.js'))
-		.pipe(gulp.dest(config.javascript.destination))
-		.pipe(concat(config.javascript.output.filename + '.min.js'))
-		.pipe(uglify().on('error', function(err) {
-			log(colors.red('[Error]'), err.toString());
-		}))
-		.pipe(gulp.dest(config.javascript.destination)
-			.on('error', function(err) {
+	if (fs.existsSync(config.javascript.source + config.javascript.input)) {
+		return gulp
+			.src(config.javascript.source + config.javascript.input)
+			.pipe(webpackStream({
+				output: {
+					filename: config.javascript.output.filename + '.js',
+					library: config.javascript.output.name,
+					libraryTarget: config.javascript.output.format,
+					libraryExport: 'default'
+				},
+				module: {
+					rules: [
+						{
+							test: /\.(js|jsx)$/,
+							exclude: /(node_modules)/,
+							loader: 'babel-loader',
+							options: {
+								babelrc: './babelrc'
+							}
+						},
+					],
+				}
+			}), webpack)
+			.pipe(concat(config.javascript.output.filename + '.js'))
+			.pipe(gulp.dest(config.javascript.destination))
+			.pipe(concat(config.javascript.output.filename + '.min.js'))
+			.pipe(uglify().on('error', function(err) {
 				log(colors.red('[Error]'), err.toString());
-			})
-		);
+			}))
+			.pipe(gulp.dest(config.javascript.destination)
+				.on('error', function(err) {
+					log(colors.red('[Error]'), err.toString());
+				})
+			);
+	} else {
+		return gulp.src('.').pipe(nop());
+	}
 });
 
 gulp.task('clean:scripts', function() {
