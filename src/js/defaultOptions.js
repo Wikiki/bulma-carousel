57570faacead68358b262496d47bcb239aed1cfc
Gaetan <gaetan@hexadog.com>
2019-03-18 19:45:32 +0100

    fix(navigation): Remove icons assets references
    fix #70

--- src/js/defaultOptions.js
+++ src/js/defaultOptions.js
@@ -39,10 +39,10 @@ const defaultOptions = {
   onReady: null,
   icons: {
     'previous': `<svg viewBox="0 0 50 80" xml:space="preserve">
-      <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
+      <polyline fill="currentColor" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
     </svg>`,
     'next': `<svg viewBox="0 0 50 80" xml:space="preserve">
-      <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
+      <polyline fill="currentColor" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
     </svg>`
   }
 };
